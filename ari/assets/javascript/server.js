3943e0831ad294274c11b092ab79d28556175c87
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-07 11:04:43 -0400

    added empty tables variable

--- ari/assets/javascript/server.js
+++ ari/assets/javascript/server.js
@@ -14,6 +14,8 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+var tables=[]
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +23,10 @@ app.use(express.json());
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
   });
+
+  app.push("/")
+
+  
 
 
 
