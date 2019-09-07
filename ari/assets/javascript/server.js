0953d722b627372c31f75c9a7a9583b5312e65ff
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-07 10:54:40 -0400

    added ari folder

---
+++ ari/assets/javascript/server.js
@@ -0,0 +1,35 @@
+//Create express server
+//Create a set of routes for API/HTML pages
+//Read data from user input (Name, phone number, email)
+//Store data into an array of objects
+//Indicate whether they have one of 5 tables or if they are on the waiting list
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
